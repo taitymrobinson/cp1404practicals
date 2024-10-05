9d12af6c42ef04fbfda64ac8d4c975ee425959c3
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-06 09:02:55 +1000

    Answer question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,7 +1,15 @@
 # Question 1
-
 FILENAME = "name.txt"
 name = input("Enter your name: ")
 in_file = open(FILENAME, "w")
 print(name, file=in_file)
 in_file.close()
+
+# Question 2
+FILENAME = "name.txt" # This is repeated as instructions state to treat as separate programs.
+in_file = open(FILENAME, "r")
+for name in in_file:
+    print(f"Hi {name.strip()}!")
+in_file.close()
+
+# Question 3
