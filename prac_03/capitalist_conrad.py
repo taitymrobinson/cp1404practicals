ae7bc3999f11bf5db690df523e938840def2d325
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-05 17:53:08 +1000

    Update boundary values, add write to file capabilities

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,17 +9,21 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "capitalist_conrad.txt"
 
+out_file = open(FILENAME, 'w')
+number_of_days = 0
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    number_of_days += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -32,4 +36,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
