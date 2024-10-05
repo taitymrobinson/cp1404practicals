d99a3f60ec7044ed5d0b59fafc905bf6e34dee1c
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-06 08:24:47 +1000

    Complete special character search

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -47,7 +47,12 @@ def is_valid_password(password):
     if number_of_digit == 0 or number_of_lower == 0 or number_of_upper == 0:
         return False
     # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
+    if IS_SPECIAL_CHARACTER_REQUIRED:
+        for character in password:
+            if character in SPECIAL_CHARACTERS:
+                number_of_special += 1
+        if number_of_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
