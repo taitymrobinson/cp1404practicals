dd1c1c38f2466054bffea4bf49e42c950015db03
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-05 17:57:14 +1000

    Answer questions

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+    When an invalid character is entered (not an integer)
 2. When will a ZeroDivisionError occur?
+    When zero is entered as the denominator
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+    Have a get valid number function
 """
 
 try:
