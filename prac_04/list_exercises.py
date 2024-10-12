ed9eaf9c2dd15ad97d063927f37a4e18875d4145
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-13 01:39:50 +1000

    Add security checker section

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,7 +1,9 @@
-"""CP1403 PRACTICAL 04"""
+"""CP1403 PRACTICAL 04
+PART ONE - BASIC LIST FUNCTIONS"""
 NUMBER_OF_REPETITIONS_ALLOWED = 5
 repetition_counter = 0
 numbers = []
+
 while repetition_counter < NUMBER_OF_REPETITIONS_ALLOWED:
     number = int(input("Number: "))
     numbers.append(number)
@@ -12,3 +14,12 @@ print(f"Last number is: {numbers[-1]}")
 print(f"The smallest number is: {min(numbers)}")
 print(f"The largest number is: {max(numbers)}")
 print(f"The average number is: {sum(numbers) / len(numbers)}")
+
+"""PART TWO - WOEFULLY INACCURATE SECURITY CHECKER"""
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+current_username = input("What is your username? ")
+if current_username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
