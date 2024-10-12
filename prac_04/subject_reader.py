e7c02fd4da3f26be77e56f9e9aaa47ac587fb3c7
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-13 01:11:52 +1000

    Update original load data function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,6 +14,7 @@ def main():
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +24,8 @@ def load_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
-
+    return data
 
 main()
