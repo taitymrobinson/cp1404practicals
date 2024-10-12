5fb55d1e7476a3107eec7c72ef3294d897ed7d4e
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-12 19:59:01 +1000

    Answer Written questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,12 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""Questions
+numbers[0] (3) CORRECT
+numbers[-1] (2) CORRECT
+numbers[3] (1) CORRECT
+numbers[:-1] ([3, 1, 4, 1, 5, 9]) CORRECT
+numbers[3:4] ([1]) CORRECT
+5 in numbers (True) CORRECT
+7 in numbers (False) CORRECT
+"3" in numbers (False) CORRECT
+numbers + [6, 5, 3] ([3, 1, 4, 1, 5, 9, 2, 6, 5, 3]) CORRECT
+"""
