734b2146af256e124f050f4313342a6fe3b1c53a
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-13 01:35:34 +1000

    Add information output section

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,3 +7,8 @@ while repetition_counter < NUMBER_OF_REPETITIONS_ALLOWED:
     numbers.append(number)
     repetition_counter += 1
 
+print(f"First number is: {numbers[0]}")
+print(f"Last number is: {numbers[-1]}")
+print(f"The smallest number is: {min(numbers)}")
+print(f"The largest number is: {max(numbers)}")
+print(f"The average number is: {sum(numbers) / len(numbers)}")
