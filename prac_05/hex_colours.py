089377aa13f06a1f392d0b99bffb86aaf7c43c57
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-20 08:53:13 +1000

    Update input formatting to 'title'

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,13 +2,13 @@
 CP1404 - PRAC 05
 Colour Lookup
 """
-COLOUR_TO_CODE = {"Harlequin": "#3fff00", "Mauvelous": "#ef98aa", "Zomp:": "#39a78e", "Wildstrawberry": "#ff43a4",
+COLOUR_TO_CODE = {"Harlequin": "#3fff00", "Mauvelous": "#ef98aa", "Zomp:": "#39a78e", "Wild Strawberry": "#ff43a4",
                   "Vermillion": "#e34234", "Sapphire": "#0f52ba", "Rose Bonbon": "#f9429e", "Pistachio": "#93c572",
                   "Peach": "#ffe5b4", "Mandarin": "#f37a48"}
-colour = input("Enter colour: ").capitalize()
+colour = input("Enter colour: ").title()
 while colour != "":
     try:
         print(f"The hex code for {colour} is {COLOUR_TO_CODE[colour]}")
     except KeyError:
         print("Invalid colour")
-    colour = input("Enter colour: ").capitalize()
+    colour = input("Enter colour: ").title()
