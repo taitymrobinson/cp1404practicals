14fb906c569783111518334fb53244cf046f3e2a
Taitym <taitym.robinson@my.jcu.edu.au>
2024-10-19 18:42:37 +1000

    Complete prac 5 colour  lookup exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - PRAC 05
+Colour Lookup
+"""
+COLOUR_TO_CODE = {"Harlequin": "#3fff00", "Mauvelous": "#ef98aa", "Zomp:": "#39a78e", "Wildstrawberry": "#ff43a4",
+                  "Vermillion": "#e34234", "Sapphire": "#0f52ba", "Rose Bonbon": "#f9429e", "Pistachio": "#93c572",
+                  "Peach": "#ffe5b4", "Mandarin": "#f37a48"}
+colour = input("Enter colour: ").capitalize()
+while colour != "":
+    try:
+        print(f"The hex code for {colour} is {COLOUR_TO_CODE[colour]}")
+    except KeyError:
+        print("Invalid colour")
+    colour = input("Enter colour: ").capitalize()
