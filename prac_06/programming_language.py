37ab955d2d6f54c3eba9e74ed9ec29cc372fe80f
Taitym <taitym.robinson@my.jcu.edu.au>
2024-11-03 18:42:33 +1000

    Complete prac 06 intermediate exercise

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+class ProgrammingLanguage:
+    """Represents a programming language object."""
+    def __init__(self, name, typing, reflection, year):
+        """Initialises a programming language object."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing}, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determine if typing style is dynamic."""
+        return self.typing == 'Dynamic'
+
