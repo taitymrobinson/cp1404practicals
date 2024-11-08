27e13912a523b5f72df75df08bb1a2cf09182da7
Taitym <taitym.robinson@my.jcu.edu.au>
2024-11-08 17:51:12 +1000

    Complete Prac 07 - Walkthrough

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,19 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
-    def __repr__(self):
+    def __str__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, Pointer arithmetic={self.pointer_arithmetic}."
+                f" First appeared in {self.year}. ")
+
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -25,9 +28,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
 
     languages = [ruby, python, visual_basic]
     print(python)
