35f44f1b94c3efc5cda09e6d4b6c354eef1e93a3
Taitym <taitym.robinson@my.jcu.edu.au>
2024-11-18 21:36:06 +1000

    complete prac 08 - DFS Conversion pt2

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,35 +7,36 @@ Actual:
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.properties import StringProperty
 
+CONVERSION_RATE = 1.609
 
 class ConvertMilesToKilometersApp(App):
     """Convert miles to kilometers is a Kivy app for converting miles to kilometers."""
     km_output = StringProperty()
 
     def build(self):
-        """ Build the kivy app from the kv file"""
+        """Build the kivy app from the kv file."""
         self.title = 'Convert Miles to Kilometers'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert(self, value):
+    def handle_convert(self, input_text):
+        """Convert miles to kilometers."""
+        conversion = self.convert_to_number(input_text) * CONVERSION_RATE
+        self.km_output = str(conversion)
+
+    def handle_increment(self, input_text, increment):
+        """Increments input text."""
+        result = self.convert_to_number(input_text) + increment
+        self.root.ids.input_number.text = str(result)
+
+    @staticmethod
+    def convert_to_number(text):
+        """Convert text to float or 0.0 if invalid."""
         try:
-            conversion = float(value) * 1.609
-            self.root.ids.output_label.text = str(conversion)
+            return float(text)
         except ValueError:
-            pass
-
-    def handle_increment(self, value, increment):
-        try:
-            result = float(value) + increment
-            self.km_output = str(result)
-        except ValueError:
-            pass
-
-
-
+            return 0.0
 
 ConvertMilesToKilometersApp().run()
