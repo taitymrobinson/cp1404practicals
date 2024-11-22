0d52ae0efdedeb2f4750159c21a975c6fa75a5e3
Taitym <taitym.robinson@my.jcu.edu.au>
2024-11-22 18:20:14 +1000

    Add testing of Taxi class

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,20 @@
+"""
+CP1404 WALKTHROUGH
+ESTIMATE - 5 mins
+ACTUAL: 7 mins
+"""
+
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Tests the Taxi class"""
+    taxicab = Taxi("Prius 1", 100, 1.23)
+    taxicab.drive(40)
+    print(taxicab)
+    taxicab.start_fare()
+    taxicab.drive(100)
+    print(taxicab)
+
+
+main()
